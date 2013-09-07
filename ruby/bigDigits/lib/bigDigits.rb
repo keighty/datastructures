6d004289df9d1edf4560f6373f129cdec42c3139
keighty <k80.leonard@gmail.com>
2013-09-07 10:07:48 -0700

    adds solution using map

--- ruby/bigDigits/lib/bigDigits.rb
+++ ruby/bigDigits/lib/bigDigits.rb
@@ -3,13 +3,8 @@ require_relative 'digit_fixtures'
 class BigDigits
   @@digits = [ZERO, ONE]
   def show(number)
-
-    #split the number into char array
     digit_array = number.to_s.split("")
-
     line_array = Array.new(16) { "" }
-
-    #for each digit, split it on \n and add to line_array
     digit_array.each do |digit|
       big_digit = @@digits[digit.to_i]
       big_digit.split("\n").each_with_index do |line, index|
@@ -17,6 +12,19 @@ class BigDigits
       end
     end
 
+    line_array.join("\n") + "\n"
+  end
+
+  def show_map(number)
+    digit_array = number.to_s.split("")
+    line_array = Array.new(16) { "" }
+
+    digit_array.each do |digit|
+      big_digit = @@digits[digit.to_i].split("\n").reverse!
+      line_array = line_array.map do |e|
+        e += big_digit.pop
+      end
+    end
     line_array.join("\n") + "\n"
   end
 end
