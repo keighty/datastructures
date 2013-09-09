243a338bfff08a97163ca8b8b60f3d0c2e82350f
keighty <k80.leonard@gmail.com>
2013-09-09 14:28:48 -0700

    adds logic for processing input

--- ruby/filejoin/lib/filejoin.rb
+++ ruby/filejoin/lib/filejoin.rb
@@ -1,2 +1,33 @@
+require 'readline'
+
 class Filejoin
+  attr_accessor :age_hash
+
+  def initialize
+    @age_hash = Hash.new(0)
+    process_input
+  end
+
+  def process_input
+    process_string(ARGF.read)
+  end
+
+  def sorted_hash
+    Hash[*age_hash.sort.flatten]
+  end
+
+  def process_string(string)
+    string.split("\n").each do |tuple|
+      process_tuple(tuple)
+    end
+  end
+
+  def process_tuple(tuple)
+    line = tuple.split(",")
+    age_hash[line[1].strip.to_i] += 1
+  end
+
 end
+
+f = Filejoin.new
+p f.sorted_hash
