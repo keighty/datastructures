81a66eb046b24df3b57238c0937f4a80345050bb
keighty <k80.leonard@gmail.com>
2013-09-12 12:53:54 -0700

    adds test of public interface

--- ruby/filejoin/spec/filejoin_spec.rb
+++ ruby/filejoin/spec/filejoin_spec.rb
@@ -2,7 +2,16 @@ require 'spec_helper'
 
 
 describe Filejoin do
-before do
-end
-it 'should pass'
+
+  TESTFILE10 = "lib/testfile10.csv"
+  TESTFILE100 = "lib/testfile100.csv"
+  TESTFILE1000 = "lib/testfile1000.csv"
+
+  before do
+    @fj = Filejoin.new
+  end
+
+  it 'should take commandline args' do
+  end
+
 end
