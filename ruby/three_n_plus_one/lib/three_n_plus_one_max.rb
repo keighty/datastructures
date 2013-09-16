343c8ce4c9185aa5796719722417f02012fcf464
keighty <k80.leonard@gmail.com>
2013-09-16 15:39:06 -0700

    adds variable initialization and output format

--- ruby/three_n_plus_one/lib/three_n_plus_one_max.rb
+++ ruby/three_n_plus_one/lib/three_n_plus_one_max.rb
@@ -1,6 +1,54 @@
-require 'three_n_plus_one'
+require_relative './three_n_plus_one'
 
 class ThreeNPlusOneMax
-  def initialize(i, j)
+  attr_accessor :max, :input_range, :output
+
+  def initialize
+    @output = []
   end
+
+  def process(*args)
+    if args.empty?
+      process_input
+    else
+      process_string(args[0][:input])
+    end
+    output_string
+  end
+
+  def output_string
+    output.each do |tuple|
+       puts tuple.join(" ")
+    end
+  end
+
+  private
+    def process_input
+      process_string(ARGF.read)
+    end
+
+    def process_string(string)
+      string.split("\n").each do |tuple|
+        process_tuple(tuple)
+      end
+    end
+
+    def process_tuple(pair_string)
+      @input_range = pair_string.split(" ").map(&:to_i)
+      output << process_range(input_range[0]..input_range[1])
+    end
+
+    def process_range(range)
+      @max = 0
+      range.each do |number|
+        sequence(number)
+      end
+      [input_range[0], input_range[1], max]
+    end
+
+    def sequence(number)
+      tnp1 = ThreeNPlusOne.new(number)
+      temp = tnp1.sequence_number
+      @max = temp if temp > @max
+    end
 end
--- ruby/three_n_plus_one/spec/three_n_plus_one_max_spec.rb
+++ ruby/three_n_plus_one/spec/three_n_plus_one_max_spec.rb
@@ -3,6 +3,16 @@ require 'spec_helper'
 describe ThreeNPlusOneMax do
   before (:each) { @max = ThreeNPlusOneMax.new }
 
-  it "should take "
+  it "should process a line of input" do
+    STDOUT.should_receive(:puts).with("1 10 20")
+    @max.process(input: "1 10")
+  end
+
+  it "should process a file" do
+    # STDIN.should_receive(:read).and_return("string")
+    executable = File.join(File.dirname(__FILE__), '..', 'lib/three_n_plus_one_max')
+    data = File.join(File.dirname(__FILE__), '..', 'data/test_input.txt')
+    %x{ruby #{executable} #{data}}
+  end
 
 end
