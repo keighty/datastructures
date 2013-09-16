6f930ecfeed0f6d0800f51f6cdc29dfe4ca48b08
keighty <k80.leonard@gmail.com>
2013-09-16 10:41:00 -0700

    adds logic and tests for 3n+1 alg

--- ruby/three_n_plus_one/lib/three_n_plus_one.rb
+++ ruby/three_n_plus_one/lib/three_n_plus_one.rb
@@ -1,2 +1,23 @@
 class ThreeNPlusOne
+  attr_accessor :sequence
+
+  def initialize
+    @sequence = []
+  end
+
+  def make_sequence(number)
+    sequence.push(number)
+
+    return if number == 1
+
+    if number % 2 == 0
+      make_sequence(number/2)
+    else
+      make_sequence(number * 3 + 1)
+    end
+  end
+
+  def sequence_number
+    sequence.length
+  end
 end
--- ruby/three_n_plus_one/spec/three_n_plus_one_spec.rb
+++ ruby/three_n_plus_one/spec/three_n_plus_one_spec.rb
@@ -1,8 +1,17 @@
 require 'spec_helper'
 
-
 describe ThreeNPlusOne do
-  before (:each) {  }
-  subject {  }
-  xit 'should pass'
+  before (:each) { @tnp1 = ThreeNPlusOne.new }
+
+  it { should respond_to :make_sequence }
+
+  it "should return a sequence" do
+    @tnp1.make_sequence(22)
+    expect(@tnp1.sequence).to eq [22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
+  end
+
+  it "should return a sequence number" do
+    @tnp1.make_sequence(22)
+    expect(@tnp1.sequence_number).to eq 16
+  end
 end
