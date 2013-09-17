bc863e80b4735de28aba9cbd887c7ed3115bbf96
keighty <k80.leonard@gmail.com>
2013-09-17 11:19:45 -0700

    adds spec and logic for move over and move onto

--- ruby/blocks/lib/blocks.rb
+++ ruby/blocks/lib/blocks.rb
@@ -1,2 +1,57 @@
 class Blocks
+  attr_accessor :piles, :locations
+
+  def initialize(size)
+    @locations = Array.new(size) { |index| index }
+    @piles     = Array.new(size) { |index| [index] }
+  end
+
+  def show_piles
+    message = ""
+    @piles.each_with_index do |element, index|
+      message += "#{index}: #{element.join(" ")}\n"
+    end
+    message
+  end
+
+  def arm(args)
+    command  = "#{args[0]}_#{args[2]}"
+    a        = args[1].to_i
+    b        = args[3].to_i
+
+    self.send(command.to_sym, a, b)
+  end
+
+  # puts block a onto block b after returning any blocks that are stacked on top of blocks a and b to their initial positions.
+  def move_onto(a, b)
+    reveal(a)
+    reveal(b)
+    piles[locations[b]] << piles[locations[a]].pop
+    locations[a] = locations[b]
+  end
+
+  # puts block a onto the top of the stack containing block b, after returning any blocks that are stacked on top of block a to their initial positions.
+  def move_over(a, b)
+    reveal(a)
+    piles[locations[b]] << piles[locations[a]].pop
+    locations[a] = locations[b]
+  end
+
+  def pile(a, operator, b)
+  end
+
+  def quit
+  end
+
+  private
+    def reveal(block)
+      loc = locations[block]
+      top_block = piles[loc].last
+      while top_block != block
+        top_block = piles[loc].pop
+        piles[top_block] << top_block
+        top_block = piles[loc].last
+      end
+    end
+
 end
--- ruby/blocks/spec/blocks_spec.rb
+++ ruby/blocks/spec/blocks_spec.rb
@@ -1,6 +1,31 @@
 require 'spec_helper'
 
 describe Blocks do
-  before (:each) { @bg = Blocks.new }
-  it 'should pass'
+  before (:each) { @bg = Blocks.new(3) }
+  subject { @bg }
+
+  it "should output the piles of blocks" do
+    expect(@bg.show_piles).to eq "0: 0\n1: 1\n2: 2\n"
+  end
+
+  it "should move a onto b" do
+    @bg.arm(%w{move 1 onto 2})
+    expect(@bg.show_piles).to eq "0: 0\n1: \n2: 2 1\n"
+  end
+# puts block a onto the top of the stack containing block b, after returning any blocks that are stacked on top of block a to their initial positions.
+  it "should move a over b" do
+    @bg.arm(%w{move 1 onto 2})
+    @bg.arm(%w{move 2 over 0})
+    expect(@bg.show_piles).to eq "0: 0 2\n1: 1\n2: \n"
+  end
+
+  # it "should move a over b"
+  # it "should pile a onto b"
+  # it "should pile a over b"
+  # it "should ignore illegal commands"
+  # it "should quit"
+  # xit "should output the Blocks game with no trailing spaces" do
+  #   @bg.game_array = [[], [1, 0, 2], []]
+  #   expect(@bg.show_game).to eq "0:\n1: 1 0 2\n2:\n"
+  # end
 end
