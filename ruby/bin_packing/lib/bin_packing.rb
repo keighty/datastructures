9c0dfcee8aa6ead7246bdc124f103202ed3b8be5
keighty <k80.leonard@gmail.com>
2013-09-20 09:22:35 -0700

    adds implementation for bin creation

--- ruby/bin_packing/lib/bin_packing.rb
+++ ruby/bin_packing/lib/bin_packing.rb
@@ -1,2 +1,25 @@
 class BinPacking
+  attr_accessor :bins
+
+  def initialize
+    @bins = []
+  end
+
+  Bin = Struct.new(:browns, :greens, :clears, :initial)
+
+  def id_contents(input)
+    temp = input.split(" ")
+    until temp.empty?
+      bin = Bin.new
+      bin.browns = temp.shift
+      bin.greens = temp.shift
+      bin.clears = temp.shift
+      bins << bin
+    end
+  end
+
+  def flag_bins
+    # check all brown bottles
+    # flag the bin with the most brown bottles
+  end
 end
--- ruby/bin_packing/spec/bin_packing_spec.rb
+++ ruby/bin_packing/spec/bin_packing_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe BinPacking do
-  before (:each) {  }
-  xit 'should pass'
+  before (:each) { @bp = BinPacking.new }
+  subject { @bp }
+  it "should process a 9 digit string" do
+    @bp.id_contents("5 10 5 20 10 5 10 20 10")
+    p @bp.bins
+  end
+  it "should output bin ids"
+  it "should output required moves"
 end
