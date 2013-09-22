043e35a6786e09e8096caa36258af8b0ed303377
keighty <k80.leonard@gmail.com>
2013-09-22 11:49:45 -0700

    adds logic to process input

--- ruby/arbitrage/lib/arbitrage.rb
+++ ruby/arbitrage/lib/arbitrage.rb
@@ -1,2 +1,22 @@
 class Arbitrage
+  attr_accessor :n, :conversions
+  def initialize
+  end
+
+  def process_input(input=ARGF.read)
+    strings = input.split("\n")
+    @n = strings.shift.to_i
+    get_conversions(strings)
+  end
+
+  def get_conversions(strings)
+    @conversions = strings.map do |line|
+      line.split(" ").map {|x| x.to_f}
+    end
+  end
 end
+
+x = Arbitrage.new
+x.process_input
+p x.n
+p x.conversions
